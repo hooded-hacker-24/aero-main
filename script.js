93072da88f06395349d8c268b6f9cc66746bc05a
hooded-hacker-24 <omkarschaubal@gmail.com>
2023-02-12 22:26:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar')
+let videoBtn = document.querySelectorAll('.vid-btn')
+
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+    document.querySelector('.controls .active').classList.remove('active');
+    // $('.controls.active').classList. remove('active');
+    btn.classList.add('active');
+    let src = btn.getAttribute('data-src');
+    document.querySelector('#video-slider').src=src;
+    });
+});
